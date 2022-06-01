89d4e8cca76efb6094fee1deef5f474d4bafe662
Karin van Homelen <karinvanhomelen@live.nl>
2022-06-01 21:25:19 +0200

    setting up exercise

---
+++ main.py
@@ -0,0 +1,17 @@
+import sys
+import os
+from PIL import Image, ImageFilter
+
+# use terminal: python3 \Pokedex \new
+
+
+#TODO 1: grab first and second argument with sys
+
+
+#TODO 2: check if '\new' exists, if not create it )you can use os or pathlib module)
+
+
+#TODO 3: loop through pokedex
+
+
+#TODO 4: convert images to png
