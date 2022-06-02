66301464cdd255bb1c2ee846cca76cbbb3c74758
Karin van Homelen <karinvanhomelen@live.nl>
2022-06-02 17:47:26 +0200

    adding working code

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 import sys
 import os
-from PIL import Image, ImageFilter
+from PIL import Image
 
-# use terminal: python3 \Pokedex \new
+# TODO 1: grab first and second argument with sys
+parent_dir = sys.argv[1]  # should be ./Pokedex
+dest_dir = sys.argv[2]  # should be ./new
 
+# parent_dir = "./Pokedex"
+# dest_dir = "./new"
+# TODO 2: check if '\new' exists, if not create it
+if not os.path.isdir(dest_dir):
+	os.mkdir(dest_dir)
 
-#TODO 1: grab first and second argument with sys
+# TODO 3: loop through pokedex
+for file in os.listdir(parent_dir):
+	img = Image.open(f"{parent_dir}/{file}")
+	# TODO 4: convert images to png
+	file = file.replace(".jpg", ".png")
+	img.save(f"{dest_dir}/{file}")
 
-
-#TODO 2: check if '\new' exists, if not create it )you can use os or pathlib module)
-
-
-#TODO 3: loop through pokedex
-
-
-#TODO 4: convert images to png
